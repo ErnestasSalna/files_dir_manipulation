9e47213fa5254e2b095c857cb55537db70810462
Ernestas Šalna <ernestas.salna@teliacompany.com>
2024-08-31 18:10:06 +0300

    Fixing up the path feeding into ScanPath method

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"embed"
 	s "files_dir_scanner/scanner"
 	"fmt"
 	"os"
 )
-
-var exampleFS embed.FS
 
 func main() {
 	if len(os.Args) < 2 {
@@ -16,7 +13,7 @@ func main() {
 	}
 
 	path := os.Args[1] // Get path from the command line
-	entries, err := s.ScanPath(exampleFS, path)
+	entries, err := s.ScanPath(os.DirFS(path), ".")
 	if err != nil {
 		fmt.Printf("Error scanning path %s: %v\n", path, err)
 		return
